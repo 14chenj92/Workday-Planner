e211a3e7787f57b2cf086aa33a039c29ea9bfef7
J <09.chen@gmail.com>
2023-01-18 21:20:30 -0800

    added comments to js

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var timeDisplayEl = $("#currentDay");
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
+// targets the specific items to save to localStorage
 $(".saveBtn").on("click", function() {
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
 
-  // save in localStorage
   localStorage.setItem(time, text);
 });
 
@@ -39,6 +15,7 @@ timeDisplayEl.text(rightNow);
 var currenttime = dayjs();
 var currenthour = currenttime.format("H");
 
+// creates a loop to add a class by comparing the current time 
 for (i = 9; i <= 17; i++) {
   var hournumber = "#hour-" + i;
   if (currenthour < i) {
@@ -50,6 +27,7 @@ for (i = 9; i <= 17; i++) {
 }
 };
 
+// gets the item from localStorage for the text to stay after refresh
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -59,4 +37,3 @@ $("#hour-14 .description").val(localStorage.getItem("hour-14"));
 $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
